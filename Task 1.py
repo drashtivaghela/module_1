21435de1a5b1e0034bb986c130fddb736c6571a6
drashtivaghela <117993912+drashtivaghela@users.noreply.github.com>
2023-02-28 16:17:08 +0545

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,29 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
-
-
 import numpy as np
 cric_data=np.loadtxt("cric.tsv",skiprows=1)
 
-
-# In[11]:
-
-
 cric_data
-
-
-# In[21]:
-
 
 Sachin=cric_data[:,1]
 Dravid=cric_data[:,2]
 India=cric_data[:,3]
-
-
-# In[22]:
-
 
 def stats(x):
     a=np.mean(x)
@@ -31,27 +16,8 @@ def stats(x):
     b=np.median(x)
     print("Median:",b)
 
-
-# In[23]:
-
-
 stats(Sachin)
-
-
-# In[24]:
-
 
 stats(Dravid)
 
-
-# In[25]:
-
-
 stats(India)
-
-
-# In[ ]:
-
-
-
-
